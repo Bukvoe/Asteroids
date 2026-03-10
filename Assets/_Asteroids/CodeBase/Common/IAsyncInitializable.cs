df41f70beb4b00a08b15d171f4588fe663bde38f
Bukvoe <Bukvoe.com>
2026-03-10 20:12:26 +0300

    Feature 6: Add IAssetService

---
+++ Assets/_Asteroids/CodeBase/Common/IAsyncInitializable.cs
@@ -0,0 +1,9 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace _Asteroids.CodeBase.Common
+{
+    public interface IAsyncInitializable
+    {
+        UniTask InitializeAsync();
+    }
+}
---
+++ Assets/_Asteroids/CodeBase/Services/Asset/IAssetService.cs
@@ -0,0 +1,14 @@
+﻿using _Asteroids.CodeBase.Common;
+using Cysharp.Threading.Tasks;
+
+namespace _Asteroids.CodeBase.Services.Asset
+{
+    public interface IAssetService : IAsyncInitializable
+    {
+        public UniTask<T> LoadAsync<T>(string key) where T : class;
+
+        public void Release(string key);
+
+        public void ReleaseAll();
+    }
+}
