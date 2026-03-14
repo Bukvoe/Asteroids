3216f7058aca35c6e495efda1f812a3f85e41f51
Bukvoe <Bukvoe.com>
2026-03-14 15:37:26 +0300

    Refactoring: Use FirebaseInitializer in FirebaseRemoteConfigProvider

--- Assets/_Asteroids/CodeBase/Services/Analytics/IAnalyticsService.cs
+++ Assets/_Asteroids/CodeBase/Services/Analytics/IAnalyticsService.cs
@@ -1,8 +1,9 @@
+using _Asteroids.CodeBase.Common;
 using _Asteroids.CodeBase.Data;
 
 namespace _Asteroids.CodeBase.Services.Analytics
 {
-    public interface IAnalyticsService
+    public interface IAnalyticsService : IAsyncInitializable
     {
         public void TrackRunStarted();
         public void TrackRunEnded(RunStats runStats);
